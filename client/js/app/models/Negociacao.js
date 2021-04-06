8cae1841ddb56c368ec005cea6838b57171cbdf6
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-06 19:26:05 -0300

    Programação defensiva: - objetos imutáveis

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,26 +2,27 @@ class Negociacao {
     // atributos da classe
     constructor(data, quantidade, valor) {
         // declarando parâmetros no constructor
-        this._data = data
+        this._data = new Date(data.getTime())
         this._quantidade = quantidade
         this._valor = valor
+        Object.freeze(this)
     }
 
     // métodos da classe
-    obtemVolume() {
+    get volume() {
         return this._quantidade * this._valor
     }
 
     // métodos acessadores: GET. Acessam os atributos privados da classe
-    getData() {
-        return this._data
+    get data() {
+        return new Date(this._data.getTime())
     }
 
-    getQuantidade() {
+    get quantidade() {
         return this._quantidade
     }
     
-    getValor() {
+    get valor() {
         return this._valor
     }
 }
