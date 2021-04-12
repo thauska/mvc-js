6ee9502e516b46b195975fd9a6c5407709b06abf
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-12 18:25:02 -0300

    - Novo modelo para regra de lista de negociações - classes auxiliares dentro do controller de negociação

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,26 +1,35 @@
 class NegociacaoController {
+  constructor() {
+    let $ = document.querySelector.bind(document)
 
-    constructor() {
-        let $ = document.querySelector.bind(document)
-        
-        this._inputData = $('#data')
-        this._inputQntd = $('#quantidade')
-        this._inputValor = $('#valor')
+    this._inputData = $("#data")
+    this._inputQntd = $("#quantidade")
+    this._inputValor = $("#valor")
+    this._listaNegociacoes = new ListaNegociacoes()
+  }
 
-    }
+  adiciona(event) {
+    event.preventDefault(); 
 
-    adiciona(event) {
-        event.preventDefault()
+    this._listaNegociacoes.adiciona(this._criaNegociacao())
+    this._limpaFormulario()
 
-        let negociacao = new Negociacao(
-            DateHelper.textToDate(this._inputData.value) ,
-            this._inputQntd.value,
-            this._inputValor.value
-        )
+    console.log(this._listaNegociacoes.negociacoes)
+  }
 
-        console.log(negociacao)
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textToDate(this._inputData.value),
+      this._inputQntd.value,
+      this._inputValor.value
+    )
+  }
 
-        console.log(DateHelper.dateToText(negociacao.data))
+  _limpaFormulario() {
+      this._inputData.value = ""
+      this._inputQntd.value = 1
+      this._inputValor.value = 0.0
 
-        }
+      this._inputData.focus()
+  }
 }
