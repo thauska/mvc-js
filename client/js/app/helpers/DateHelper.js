18def2534230177d4aa3beb141d055c9cc964433
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-12 12:09:58 -0300

    Template String and RegEx

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,11 +5,13 @@ class DateHelper {
     }
 
     static dateToText(date) {
-        // com static, não preciso instanciar a classe, ela poderá ser invocada diretamente
-        return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
+        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}` 
     }
 
     static textToDate(text) {
+        if (!/\d{4}-\d{2}-\d{2}/.test(text))
+            throw new Error('Most be in format yyyy-mm-dd')
+
         return new Date(...text.split('-').map((item, indice) => item - indice % 2))
     }
 
