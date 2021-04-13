5457799378739d95d68beb7354bee78b4423a64b
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-13 16:32:44 -0300

    Herança e reutilização de código

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,8 +1,7 @@
-class NegociacoesView {
-
+class NegociacoesView extends View {
+    
     constructor(elemento) {
-
-        this._elemento = elemento
+        super(elemento)
     }
 
     _template(model) {
@@ -35,7 +34,4 @@ class NegociacoesView {
             </table>`
     }
 
-    update(model) {
-        this._elemento.innerHTML = this._template(model)
-    }
 }
