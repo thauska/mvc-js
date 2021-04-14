03b8f50727d8b94ae632da99214e232f9a3380a0
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-13 21:54:10 -0300

    Arrow function e seu escopo léxico

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,9 +6,7 @@ class NegociacaoController {
     this._inputQntd = $('#quantidade')
     this._inputValor = $('#valor')
 
-    this._listaNegociacoes = new ListaNegociacoes(this, function(model) {
-      this._negociacoesView.update(model)
-    })
+    this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model))
     this._negociacoesView = new NegociacoesView($('#negociacoesView'))
 
     this._mensagem = new Mensagem()
