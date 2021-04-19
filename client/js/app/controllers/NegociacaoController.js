6c828bba43aa67f1174c7952756d34ea373997ea
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-19 18:49:46 -0300

    Rest operator

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,20 +6,16 @@ class NegociacaoController {
     this._inputQntd = $('#quantidade')
     this._inputValor = $('#valor')
     
-    this._negociacoesView = new NegociacoesView($('#negociacoesView'))
-    
     this._listaNegociacoes = new Bind(
       new ListaNegociacoes(),
-      this._negociacoesView,
-      ['adiciona', 'esvazia']
+      new NegociacoesView($('#negociacoesView')),
+      'adiciona', 'esvazia'
     )
-    
-    this._mensagemView = new MensagemView($('#mensagemView'))
 
     this._mensagem = new Bind(
       new Mensagem(),
-      this._mensagemView,
-      ['texto']
+      new MensagemView($('#mensagemView')),
+      'texto'
     )
     
   }
--- client/js/app/helpers/Bind.js
+++ client/js/app/helpers/Bind.js
@@ -1,6 +1,7 @@
 class Bind {
 
-    constructor(model, view, props) {
+    // ... -> Rest operator
+    constructor(model, view, ...props) {
 
         let proxy = ProxyFactory.create(model, props, model => view.update(model))
         view.update(model)
