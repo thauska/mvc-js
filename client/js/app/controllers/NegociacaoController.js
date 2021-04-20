84e1efb9d4da8f537a874ae59fa8261e9ccecd20
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-20 13:10:40 -0300

    responseText

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -35,20 +35,15 @@ class NegociacaoController {
 
     /** configurações */
     xhr.onreadystatechange = () => {
-      /** Tipos de estados: 
-        0: requisição ainda não iniciada
-        1: conexão com o servidor estabelecida
-        2: requisição recebida
-        3: processando requisição
-        4: requisição está concluída e a resposta está pronta
-       */
       if(xhr.readyState == 4) {
         if(xhr.status == 200) {
 
           console.log('Obtendo as negociações do servidor')
-
+          console.log(JSON.parse(xhr.responseText))
+          
         } else {
           console.log('Não foi possível obter negociações do servidor')
+          console.log(JSON.parse(xhr.responseText))
         }
       }
     }
