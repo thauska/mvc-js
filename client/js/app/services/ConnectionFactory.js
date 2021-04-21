abbd09b80b606d0347824451a31ee82a6d7ee5d6
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-21 17:42:54 -0300

    Criando stores

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -9,15 +9,32 @@ class ConnectionFactory {
     }
 
     static getConnection() {
+
         return new Promise((resolve, reject) => {
             
             let openRequest = window.indexedDB.open(dbName, version)
 
-            openRequest.onupgradeneeded = e => {
-                
+            openRequest.onupgradeneeded = e => ConnectionFactory._createStores(e.target.result)
+
+            openRequest.onsuccess = e => {
+                console.log(e.target.result)
+                resolve(e.target.result)
             }
-            openRequest.onsuccess = e => {}
-            openRequest.onerror = e => {}
+
+            openRequest.onerror = e => {
+                console.log(e.target.error)
+                reject(e.target.error.name)
+            }
+
+        })
+    }
+
+    static _createStores(connection) {
+        stores.forEach(store => {
+
+            if(connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store)
+
+            connection.createObjectStore(store, { autoIncrement: true })
         })
     }
 }
