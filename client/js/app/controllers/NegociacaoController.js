c5549f418a145cf8e99fdea3210e4dd41042726d
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-21 20:15:41 -0300

    Removendo todas as negociações com método apagaTodos

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -91,8 +91,16 @@ class NegociacaoController {
 
   apaga() {
 
-      this._listaNegociacoes.esvazia()
-      this._mensagem.texto = 'Negociações apagadas com sucesso'
+    ConnectionFactory
+      .getConnection()
+      .then(connection => new NegociacaoDao(connection))
+      .then(dao => dao.apagaTodos())
+      .then(mensagem => {
+        this._mensagem.texto = mensagem
+        this._listaNegociacoes.esvazia()
+      })
+      .catch(error => this._mensagem.texto = error)
+
   }
 
   ordena(coluna) {
