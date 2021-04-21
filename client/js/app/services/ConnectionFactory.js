c3cfdedb363828312c7c96a8e5bf2ef7452478c7
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-21 17:54:08 -0300

    Resolvendo problema de variáveis em escopo global, utilizando Module Pattern - Todo script foi transformado em um módulo

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,40 +1,46 @@
-var stores = ['negociacoes']
-var version = 4
-var dbName = 'aluraframe'
+var ConnectionFactory = (function () {
+    // função autoinvocada
 
-class ConnectionFactory {
+  var stores = ["negociacoes"]
+  var version = 4
+  var dbName = "aluraframe"
+
+  var connection = null
+
+  return class ConnectionFactory {
 
     constructor() {
-        throw new Error('Não é possível criar instâncias de ConnectionFactory')
+      throw new Error("Não é possível criar instâncias de ConnectionFactory")
     }
 
     static getConnection() {
+      return new Promise((resolve, reject) => {
+        let openRequest = window.indexedDB.open(dbName, version)
 
-        return new Promise((resolve, reject) => {
-            
-            let openRequest = window.indexedDB.open(dbName, version)
+        openRequest.onupgradeneeded = (e) =>
+          ConnectionFactory._createStores(e.target.result)
 
-            openRequest.onupgradeneeded = e => ConnectionFactory._createStores(e.target.result)
+        openRequest.onsuccess = (e) => {
+          if (!connection) connection = e.target.result
+          // recebe conexão já existente ou uma que acabou de ser criada
 
-            openRequest.onsuccess = e => {
-                console.log(e.target.result)
-                resolve(e.target.result)
-            }
+          resolve(connection)
+        }
 
-            openRequest.onerror = e => {
-                console.log(e.target.error)
-                reject(e.target.error.name)
-            }
-
-        })
+        openRequest.onerror = (e) => {
+          console.log(e.target.error)
+          reject(e.target.error.name)
+        }
+      })
     }
 
     static _createStores(connection) {
-        stores.forEach(store => {
+      stores.forEach((store) => {
+        if (connection.objectStoreNames.contains(store))
+          connection.deleteObjectStore(store)
 
-            if(connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store)
-
-            connection.createObjectStore(store, { autoIncrement: true })
-        })
+        connection.createObjectStore(store, { autoIncrement: true })
+      })
     }
-}
+  }
+})()
