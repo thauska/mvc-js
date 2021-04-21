b3cda864b81899566ce9d8794644e2bfd7f65086
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-21 18:24:49 -0300

    Utilizando Monkey Patch, que força a modificação da API -  evita de a conexão seja fechada de forma direta

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,16 +1,17 @@
 var ConnectionFactory = (function () {
     // função autoinvocada
 
-  var stores = ["negociacoes"]
-  var version = 4
-  var dbName = "aluraframe"
+  const stores = ['negociacoes']
+  const version = 4
+  const dbName = 'aluraframe'
 
   var connection = null
+  var close = null
 
   return class ConnectionFactory {
 
     constructor() {
-      throw new Error("Não é possível criar instâncias de ConnectionFactory")
+      throw new Error('Não é possível criar instâncias de ConnectionFactory')
     }
 
     static getConnection() {
@@ -21,8 +22,13 @@ var ConnectionFactory = (function () {
           ConnectionFactory._createStores(e.target.result)
 
         openRequest.onsuccess = (e) => {
-          if (!connection) connection = e.target.result
-          // recebe conexão já existente ou uma que acabou de ser criada
+          if (!connection) {
+            connection = e.target.result
+            close = connection.close.bind(connection)
+            connection.close = function() {
+                throw new Error('Você não pode fechar diretamente a conexão')
+            }
+          }
 
           resolve(connection)
         }
@@ -41,6 +47,13 @@ var ConnectionFactory = (function () {
 
         connection.createObjectStore(store, { autoIncrement: true })
       })
+    }
+
+    static closeConnection() {
+        if(connection) {
+            close()
+            connection = null
+        }
     }
   }
 })()
