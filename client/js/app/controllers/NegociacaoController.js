f65be01406db2bc2f9ee5c15b9d1ce1356aec927
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-04-20 21:09:28 -0300

    Separando responsabilidades

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -30,28 +30,15 @@ class NegociacaoController {
   }
 
   importaNegociacoes() {
-    let xhr = new XMLHttpRequest()
-    xhr.open('GET', 'negociacoes/semana')
-
-    /** configurações */
-    xhr.onreadystatechange = () => {
-      if(xhr.readyState == 4) {
-        if(xhr.status == 200) {
-          
-          JSON.parse(xhr.responseText)
-            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-            .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
-
-          this._mensagem.texto = 'Negociações importadas com sucesso'
-          
-        } else {
-          console.log(xhr.responseText)
-          this._mensagem.texto = 'Não foi possível obter negociações do servidor'
-        }
+    let service = new NegociacaoServices()
+    service.obterNegociacaoSemana((err, negociacoes) => {
+      if(err) {
+        this._mensagem.texto = err
+        return
       }
-    }
-
-    xhr.send()
+      negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
+      this._mensagem.texto = 'Negociações importadas com sucesso!'
+    })
   }
 
   _criaNegociacao() {
